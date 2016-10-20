ae49c750eec9e17f2da683f0ad91198615b5c9ea
ChathumikaAlwis <Chathumikaalwis@gmail.com>
2016-10-20 20:15:04 +0530

    Add site.php

---
+++ site.php
@@ -0,0 +1,23 @@
+<html>
+<head>
+<style>
+#side{float:left;height:560;width:14%}
+#mid{float:right;height:113%;width:85.5%}
+#foot{float:left}
+</style>
+</head>
+<body>
+<div id="head">
+<?php include_once("headsu1.php"); ?>
+</div>
+<div id="side">
+<?php include_once("sidebar.php"); ?>
+</div>
+<div id="mid">
+<?php include_once("home.php"); ?>
+</div>
+<div id="foot" >
+<?php include_once("footer.php"); ?>
+</div>
+</body>
+</html>
