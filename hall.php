da0a1e5c8603cee66f642dfd4d96476232943dba
RaveenBouy <raveenshenal@gmail.com>
2016-10-21 14:14:39 +0530

    Add files via upload
    Hall - php

---
+++ hall.php
@@ -0,0 +1,18 @@
+<html>
+<head>
+<style>
+#foot{float:left}
+</style>
+</head>
+<body>
+<div id="head">
+<?php include_once("headsu.php"); ?>
+</div>
+<div id="mid">
+<?php include_once("hall2.php"); ?>
+</div>
+<div id="foot">
+<?php include_once("footer.php"); ?>
+</div>
+</body>
+</html>
