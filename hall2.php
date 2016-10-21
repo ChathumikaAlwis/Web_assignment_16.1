6970725bd249444ee27de17f1adf88d8f2c0367c
RaveenBouy <raveenshenal@gmail.com>
2016-10-21 14:16:35 +0530

    Add files via upload

---
+++ hall2.php
@@ -0,0 +1,259 @@
+<html>
+<head>
+<style>
+
+.name{width:170}
+td{text-align:center;color:#000000}
+</style>
+
+</head>
+<body bgcolor="#b9f6df">
+
+<table border="1px" width ="1350">
+<th bgcolor="#66e0ff"><h3>MAY 1st Week</h3></th>
+<th bgcolor="#66e0ff"><h3>MAY 2nd Week</h3></th>
+<tr>
+<td>
+<table >
+	<th class="name" bgcolor="#6600ff">Day</th>
+	<th class="name" bgcolor="#6600ff">Batch Name</th>
+	<th class="name" bgcolor="#6600ff">Hall no.</th>
+	<th class="name"bgcolor="#6600ff">Time</th>
+	
+		<tr bgcolor="#668cff">
+			<td>Monday</td>
+			<td>16.1 Computing</td>
+			<td>Hall 4</td>
+			<td>9am-4pm</td>
+		</tr>
+		<tr bgcolor="#668cff">
+			<td>Tuesday</td>
+			<td>16.2 Computing</td>
+			<td>Hall 3</td>
+			<td>9am-4pm</td>
+		</tr>
+                <tr bgcolor="#668cff">
+			<td>Wednesday</td>
+			<td>15.1 Computing</td>
+			<td>Hall 4</td>
+			<td>12am-4pm</td>
+		</tr>
+		<tr bgcolor="#668cff">	<td>Thursday</td>
+			<td>14.1 Computing</td>
+			<td>Hall 1</td>
+			<td>9am-4pm</td>
+		</tr>
+		<tr bgcolor="#668cff">
+			<td>Friday</td>
+			<td>15.2 Computing</td>
+			<td>Hall 2</td>
+			<td>12am-4pm</td>
+		</tr>
+	
+</table>
+</td>
+<td>
+<table >	
+	<th class="name" bgcolor="#6600ff">Day</th>
+	<th class="name" bgcolor="#6600ff">Batch Name</th>
+	<th class="name" bgcolor="#6600ff">Hall no.</th>
+	<th class="name"bgcolor="#6600ff">Time</th>
+	
+			<tr bgcolor="#668cff">
+			<td>Monday</td>
+			<td>14.1 Computing</td>
+			<td>Hall 4</td>
+			<td>9am-4pm</td>
+		</tr>
+		<tr bgcolor="#668cff">
+			<td>Tuesday</td>
+			<td>15.2 Computing</td>
+			<td>Hall 3</td>
+			<td>12am-4pm</td>
+		</tr>
+                <tr bgcolor="#668cff">
+			<td>Wednesday</td>
+			<td>16.1 Computing</td>
+			<td>Hall 4</td>
+			<td>12am-4pm</td>
+		</tr>
+		<tr bgcolor="#668cff">	<td>Thursday</td>
+			<td>16.2 Computing</td>
+			<td>Hall 1</td>
+			<td>9am-4pm</td>
+		</tr>
+		<tr bgcolor="#668cff">
+			<td>Friday</td>
+			<td>14.1 Computing</td>
+			<td>Hall 2</td>
+			<td>9am-4pm</td>
+		</tr>
+</table>
+</td>
+</tr>
+<th bgcolor="#66e0ff"> <h3>MAY 3rd Week</h3></th>
+<th bgcolor="#66e0ff"> <h3>MAY 4th Week</h3></th>
+<tr>
+<td>
+<table >
+	<th class="name" bgcolor="#6600ff">Day</th>
+	<th class="name" bgcolor="#6600ff">Batch Name</th>
+	<th class="name" bgcolor="#6600ff">Hall no.</th>
+	<th class="name"bgcolor="#6600ff">Time</th>
+
+			<tr bgcolor="#668cff">
+			<td>Monday</td>
+			<td>14.1 Computing</td>
+			<td>Hall 4</td>
+			<td>12am-4pm</td>
+		</tr>
+		<tr bgcolor="#668cff">
+			<td>Tuesday</td>
+			<td>15.2 Computing</td>
+			<td>Hall 3</td>
+			<td>9am-12pm</td>
+		</tr>
+                <tr bgcolor="#668cff">
+			<td>Wednesday</td>
+			<td>15.1 Computing</td>
+			<td>Hall 4</td>
+			<td>9am-4pm</td>
+		</tr>
+		<tr bgcolor="#668cff">	<td>Thursday</td>
+			<td>16.1 Computing</td>
+			<td>Hall 1</td>
+			<td>9am-4pm</td>
+		</tr>
+		<tr bgcolor="#668cff">
+			<td>Friday</td>
+			<td>16.1 Computing</td>
+			<td>Hall 2</td>
+			<td>12am-4pm</td>
+		</tr>
+	
+</table>
+</td>
+<td>
+<table >
+	<th class="name" bgcolor="#6600ff">Day</th>
+	<th class="name" bgcolor="#6600ff">Batch Name</th>
+	<th class="name" bgcolor="#6600ff">Hall no.</th>
+	<th class="name"bgcolor="#6600ff">Time</th>
+	
+			<tr bgcolor="#668cff">
+			<td>Monday</td>
+			<td>16.1 Computing</td>
+			<td>Hall 4</td>
+			<td>9am-4pm</td>
+		</tr>
+		<tr bgcolor="#668cff">
+			<td>Tuesday</td>
+			<td>15.1 Computing</td>
+			<td>Hall 3</td>
+			<td>12am-4pm</td>
+		</tr>
+                <tr bgcolor="#668cff">
+			<td>Wednesday</td>
+			<td>14.1 Computing</td>
+			<td>Hall 4</td>
+			<td>9am-12pm</td>
+		</tr>
+		<tr bgcolor="#668cff">	<td>Thursday</td>
+			<td>14.2 Computing</td>
+			<td>Hall 1</td>
+			<td>9am-12pm</td>
+		</tr>
+		<tr bgcolor="#668cff">
+			<td>Friday</td>
+			<td>16.1 Computing</td>
+			<td>Hall 2</td>
+			<td>9am-4pm</td>
+		</tr>
+</table>
+</td>
+</tr>
+<th bgcolor="#66e0ff"> <h3>JUNE 1st Week</h3></th>
+<th bgcolor="#66e0ff"> <h3>JUNE 2nd Week</h3></th>
+<tr>
+<td>
+<table >
+	<th class="name" bgcolor="#6600ff">Day</th>
+	<th class="name" bgcolor="#6600ff">Batch Name</th>
+	<th class="name" bgcolor="#6600ff">Hall no.</th>
+	<th class="name"bgcolor="#6600ff">Time</th>
+
+		<tr bgcolor="#668cff">
+			<td>Monday</td>
+			<td>15.1 Computing</td>
+			<td>Hall 4</td>
+			<td>12am-4pm</td>
+		</tr>
+		<tr bgcolor="#668cff">
+			<td>Tuesday</td>
+			<td>16.2 Computing</td>
+			<td>Hall 3</td>
+			<td>9am-4pm</td>
+		</tr>
+                <tr bgcolor="#668cff">
+			<td>Wednesday</td>
+			<td>15.1 Computing</td>
+			<td>Hall 4</td>
+			<td>12am-4pm</td>
+		</tr>
+		<tr bgcolor="#668cff">	<td>Thursday</td>
+			<td>16.1 Computing</td>
+			<td>Hall 1</td>
+			<td>9am-4pm</td>
+		</tr>
+		<tr bgcolor="#668cff">
+			<td>Friday</td>
+			<td>14.1 Computing</td>
+			<td>Hall 2</td>
+			<td>9am-12pm</td>
+		</tr>
+	
+</table>
+</td>
+<td>
+<table >
+	<th class="name" bgcolor="#6600ff">Day</th>
+	<th class="name" bgcolor="#6600ff">Batch Name</th>
+	<th class="name" bgcolor="#6600ff">Hall no.</th>
+	<th class="name"bgcolor="#6600ff">Time</th>
+	
+			<tr bgcolor="#668cff">
+			<td>Monday</td>
+			<td>15.1 Computing</td>
+			<td>Hall 4</td>
+			<td>9am-4pm</td>
+		</tr>
+		<tr bgcolor="#668cff">
+			<td>Tuesday</td>
+			<td>14.1 Computing</td>
+			<td>Hall 3</td>
+			<td>9am-4pm</td>
+		</tr>
+                <tr bgcolor="#668cff">
+			<td>Wednesday</td>
+			<td>16.2 Computing</td>
+			<td>Hall 4</td>
+			<td>9am-4pm</td>
+		</tr>
+		<tr bgcolor="#668cff">	<td>Thursday</td>
+			<td>16.1 Computing</td>
+			<td>Hall 1</td>
+			<td>9am-4pm</td>
+		</tr>
+		<tr bgcolor="#668cff">
+			<td>Friday</td>
+			<td>14.2 Computing</td>
+			<td>Hall 2</td>
+			<td>9am-4pm</td>
+		</tr>
+</table>
+</td>
+</tr>
+</table>
+
+</body>
+</html>
---
+++ studentevent.php
@@ -0,0 +1,56 @@
+<html>
+       <head>
+	   <br>
+            <title> Student Activities </title>
+        </head>
+      <body style="background-color:lightblue">
+            <b> <h1 style="color:darkblue ;text-align:center;font-family:arial;font-size:30px"><u>Student Activities</u></h1></b>
+	        <p>   Student activities or campus activities is a broad term describing most student-focused extracurricular clubs and programs at a university. Student activities are generally designed to allow students to get more involved and offer them opportunities in leadership, social responsibility, citizenship, volunteerism, and student employment. These activities are typically overseen by a Director of Student Activities, Student Affairs, or Student Engagement who often holds a master's degree in student development or a comparable field. Program directors often provide guidance for clubs, set standards for student run organizations, and can also help network with other institutions through organizations.</p>
+	  
+	<hr>
+	    <table style="width:1 00%">
+    <tr>
+       <td> <img src="StudentActivity.png" width="300" height="200"> </td>                         
+		<td> <h3 style="text-align:center"><b>Academic</b></h3> 
+	  <p>Academic student activities refer to clubs and programming specifically focusing on scholarly endeavors. This can take the form of major-based or area of study-based clubs or programs and events designed to educate students in any scholarly subject matter.
+
+			</td>
+    </tr>
+        </table><hr>
+		
+		   <table style="width:1 00%">
+    <tr>
+       <td> <img src="new.jpg" width="300" height="200"> </td>
+        <td><h3 style="text-align:center"><b>Student-Run Businesses</b></h3>
+        <p> Student-run businesses can be any sort of business that has an affiliation with the college or university and is almost completely, if not completely, run by student workers.
+
+		
+		</tr>
+         </table><hr>
+		 
+		 
+		   <table style="width:1 00%">
+    <tr>
+       <td> <img src="new2.jpg" width="300" height="200"> </td>
+        <td><h3 style="text-align:center"><b>Recreational</b></h3>
+		 <p>Recreational activities refer to clubs and programming that involve some form of physical activity.
+Recreation is an activity of leisure, leisure being discretionary time. The "need to do something for recreation" is an essential element of human biology and psychology.
+
+			</td>
+		
+		</tr>
+         </table>
+		 <hr>
+		  <table style="width:1 00%">
+    <tr>
+       <td> <img src="new3.jpg" width="300" height="200"> </td>                         
+		<td> <h3 style="text-align:center"><b>Religious and Spiritual</b></h3> 
+	  <p>Religious/spiritual activities refer to clubs and programming that allow students to connect with other students of similar faiths, practice their chosen faith, and/or learn about other faiths. Many universities have a faith center where these sorts of programs take place.
+
+			</td>
+    </tr>
+        </table>
+	  
+	  </body>
+</html>
+	    
