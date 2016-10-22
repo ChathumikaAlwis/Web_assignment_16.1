79a041d1e58bce4ef4a128dd1d4cf8e76f741af3
chanakaroshane <chanaka.778328332@gmail.com>
2016-10-22 16:05:34 +0530

    Add files via upload

---
+++ upc.php
@@ -0,0 +1,95 @@
+<html>
+<head>
+</head>
+<body bgcolor=" #ccf5ff">
+<br/>
+<marquee scrollamount="15">
+<img src="90.jpg" alt="Image" style="width:300px;height:300px;">
+<img src="91.jpg" alt="Image" style="width:300px;height:300px;">
+<img src="92.jpg" alt="Image" style="width:300px;height:300px;">
+<img src="93.jpg" alt="Image" style="width:300px;height:300px;">
+<img src="94.jpg" alt="Image" style="width:300px;height:300px;">
+<img src="95.jpg" alt="Image" style="width:300px;height:300px;">
+<img src="96.jpg" alt="Image" style="width:300px;height:300px;">
+<img src="97.jpg" alt="Image" style="width:300px;height:300px;">
+<img src="98.jpg" alt="Image" style="width:300px;height:300px;">
+<img src="99.jpg" alt="Image" style="width:300px;height:300px;">
+<img src="100.jpg" alt="Image" style="width:300px;height:300px;">
+<img src="101.jpeg" alt="Image" style="width:300px;height:300px;">
+</marquee>
+<br/>
+<br/>
+<div style="background-color: #80e5ff; height:250px">
+<div style="float:left">
+<img src="71.jpg" alt="Image" style="width:300px;height:200px">
+</div>
+<div style="float:left; margin-left:200px">
+<img src="72.jpg" alt="Image" style="width:200px;height:200px;">
+</div>
+<div style="float:left; margin-left:200px">
+<h3>Our annual new year festival is held in<br/> 27th April at Wenew stadium at 8:00am</h3> 
+</div>
+</div>
+<br/>
+<div style="background-color:#80e5ff; height:250px">
+<div style="float:left">
+<img src="77.jpg" alt="Image" style="width:300px;height:200px;">
+</div>
+<div style="float:left; margin-left:200px">
+<img src="73.jpg" alt="Image" style="width:200px;height:200px;">
+</div>
+<div style="float:left; margin-left:200px">
+<h3>Our annual open day event for all who like to <br/>see our university</h3> 
+</div>
+</div>
+<br/>
+<div style="background-color:#80e5ff; height:250px">
+<div style="float:left">
+<img src="78.jpeg" alt="Image" style="width:300px;height:200px;">
+</div>
+<div style="float:left; margin-left:200px">
+<img src="80.jpg" alt="Image" style="width:200px;height:200px;">
+</div>
+<div style="float:left; margin-left:200px">
+<h3>The 70th anniversary of our university <br/>is held grandly with a special program in <br/>the morning at 9:00am</h3> 
+</div>
+</div>
+<br/>
+<div style="background-color:#80e5ff; height:250px">
+<div style="float:left">
+<img src="84.jpg" alt="Image" style="width:300px;height:200px;">
+</div>
+<div style="float:left; margin-left:200px">
+<img src="74.jpg" alt="Image" style="width:200px;height:200px;">
+</div>
+<div style="float:left; margin-left:200px">
+<h3>The opening ceremony of our new computing <br/>building will be held at 10:00am by the <br/>exellency the president</h3> 
+</div>
+</div>
+<br/>
+<div style="background-color:#80e5ff; height:250px">
+<div style="float:left">
+<img src="85.jpg" alt="Image" style="width:300px;height:200px;">
+</div>
+<div style="float:left; margin-left:200px">
+<img src="83.jpg" alt="Image" style="width:200px;height:200px;">
+</div>
+<div style="float:left; margin-left:200px">
+<h3>Our annual exibition will be held on 5th May <br/> which will be opened to the public,<br/> and will continue for a whole week.</h3> 
+</div>
+</div>
+<br/>
+<div style="background-color:#80e5ff; height:250px">
+<div style="float:left">
+<img src="86.jpg" alt="Image" style="width:300px;height:200px;">
+</div>
+<div style="float:left; margin-left:200px">
+<img src="81.jpg" alt="Image" style="width:200px;height:200px;">
+</div>
+<div style="float:left; margin-left:200px">
+<h3>Our university's sport fiesta and competition  <br/>will be held on 30th at 9:00am </h3> 
+</div>
+</div>
+
+</body>
+</html>
