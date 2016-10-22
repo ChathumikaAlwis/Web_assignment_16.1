c950bf4e3b7d26b52405cb95ab1bdd027e8f545f
shehanhassim <shehan.hassim@yahoo.com>
2016-10-22 18:58:15 +0530

    Add files via upload

---
+++ social.php
@@ -0,0 +1,60 @@
+ <html>
+       <head>
+            <title> Society </title>
+        <style>
+	
+		
+		</style>
+		</head>
+      <body style="background-color:lightblue">
+      <b><h1 style="color:darkblue ;font-family:arial;font-size:30px"><u>Student Society</u></h1></b> 
+	  
+	  <p>A student society, student association, university society or
+	  student organization is a society or an organization, operated by students at a
+	  university or a college institution, whose membership typically consists only of students and/or alumni.<br>
+</p>
+ <hr>
+ <table style="width:100%">
+    <tr>
+       <td> <img src="mozillasmobi.jpg" width="300" height="200" </td>                         
+		<td> <h3><b>Mozilla Firefox Society</b></h3> 
+		<p>In our mozilla firefox Society we offer undergrats a unique experiance of working
+		with people and communities all across mozilla.The seniors are focused on fueling the open internet movement.</p>
+		
+        </td>		
+	</tr> 
+	</table> <hr>
+  
+     <table style="width:100%"> 
+         <tr>
+             <td> <img src="1-f.jpg" width="300" height="200"> </td>        
+             <td style="text-align:left"> <h3><b>Geek Club</b></h3>
+            <p>Unique. Different. We embrace it!<br> Today's visionaries, dream makers, achievers, inventors and greatest thinkers were yesterday's geeks and nerds.<br> We're next.
+			
+			To create a community of understanding and acceptance.
+
+            We're a community of all kinds-autistic, neurotypical, special needs-and we like you just the way you are. If you want to be a part of the club you're in!</p>
+            
+			</td>
+			
+   </tr>
+   </table> <hr>
+   
+   
+     <table style="width:100%"> 
+	    <tr>
+             <td> <img src="SportsClubsTitle.jpg" width="300" height="200"> </td>        
+             <td style="text-align:left"> <h3><b>Sports Club</b></h3>
+			 <p> Help each student succeed on and off the field  
+			 <br/>Create a positive environment for learning 
+			 <br/> Develop a cross training curriculum for healthy students.
+			 <br/> Strong community spirit and involvement
+			  Increase self esteem and physical fitness Through
+			 a well- managed Professional training program, help each student 
+			 athlete succeed on and off the field in a positive environment 
+			 increasing self- esteem and physical fitness.  </p>
+            </td>
+	 </table>	
+<br/>
+<br/>	
+ </html>
