3f217ea5ec679d1446a7c1cfa4c766184ab4f214
shehanhassim <shehan.hassim@yahoo.com>
2016-10-23 21:06:49 +0530

    Add files via upload

---
+++ gallery.php
@@ -0,0 +1,19 @@
+<html>
+<head>
+<style>
+
+#foot{float:left}
+</style>
+</head>
+<body>
+<div id="head">
+<?php include_once("headsu.php"); ?>
+</div>
+<div id="mid">
+<?php include_once("gallerycode.php"); ?>
+</div>
+<div id="foot" >
+<?php include_once("footer.php"); ?>
+</div>
+</body>
+</html>
---
+++ gallerycode.php
@@ -0,0 +1,61 @@
+<html>
+<body bgcolor="#99ccff">
+
+<div style="background-color:#80bfff"><h1>GRADUATION 2015</h3></div>
+<div style="background-color:#99c2ff">
+<img src="13.jpg" alt="picture" style="width:400px;height:400px;">
+<img src="14.jpg" alt="picture" style="width:400px;height:400px;">
+<img src="20.jpg" alt="picture" style="width:400px;height:400px;">
+<img src="21.jpg" alt="picture" style="width:400px;height:400px;">
+<img src="22.jpg" alt="picture" style="width:400px;height:400px;">
+<img src="23.jpg" alt="picture" style="width:400px;height:400px;">
+<img src="24.jpg" alt="picture" style="width:400px;height:400px;">
+<img src="43.jpg" alt="picture" style="width:400px;height:400px;">
+<img src="96.jpg" alt="picture" style="width:400px;height:400px;">
+</div>
+
+<div style="background-color:#80bfff"><h1>BATCH PARTY 2016</h3></div>
+<div style="background-color:#99c2ff">
+<img src="25.jpg" alt="picture" style="width:400px;height:400px;">
+<img src="26.jpg" alt="picture" style="width:400px;height:400px;">
+<img src="27.jpg" alt="picture" style="width:400px;height:400px;">
+<img src="28.jpg" alt="picture" style="width:400px;height:400px;">
+<img src="29.jpg" alt="picture" style="width:400px;height:400px;">
+<img src="33.jpg" alt="picture" style="width:400px;height:400px;">
+<img src="30.jpg" alt="picture" style="width:400px;height:400px;">
+<img src="31.jpg" alt="picture" style="width:400px;height:400px;">
+<img src="32.jpg" alt="picture" style="width:400px;height:400px;">
+</div>
+
+<div style="background-color:#80bfff"><h1>SPECIAL LECTURES</h3></div>
+<div style="background-color:#99c2ff">
+<img src="33.jpg" alt="picture" style="width:400px;height:400px;">
+<img src="34.jpg" alt="picture" style="width:400px;height:400px;">
+<img src="35.jpg" alt="picture" style="width:400px;height:400px;">
+<img src="36.png" alt="picture" style="width:400px;height:400px;">
+<img src="37.jpg" alt="picture" style="width:400px;height:400px;">
+<img src="38.jpg" alt="picture" style="width:400px;height:400px;">
+<img src="39.jpg" alt="picture" style="width:400px;height:400px;">
+<img src="40.jpeg" alt="picture" style="width:400px;height:400px;">
+<img src="41.jpg" alt="picture" style="width:400px;height:400px;">
+</div>
+
+
+<div style="background-color:#80bfff"><h1>HALLOWEEN PARTY</h3></div>
+<div style="background-color:#99c2ff">
+<img src="58.jpg" alt="picture" style="width:400px;height:400px;">
+<img src="51.jpg" alt="picture" style="width:400px;height:400px;">
+<img src="52.jpg" alt="picture" style="width:400px;height:400px;">
+<img src="53.jpg" alt="picture" style="width:400px;height:400px;">
+<img src="54.jpg" alt="picture" style="width:400px;height:400px;">
+<img src="55.jpg" alt="picture" style="width:400px;height:400px;">
+<img src="56.jpg" alt="picture" style="width:400px;height:400px;">
+<img src="57.jpg" alt="picture" style="width:400px;height:400px;">
+<img src="50.jpg" alt="picture" style="width:400px;height:400px;">
+</div>
+
+
+
+
+</body>
+</html>
